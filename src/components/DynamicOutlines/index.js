f6635eb24a585d7f04c2df0f04632d87c83cba26
Anthony Castrio <anthonycastrio@gmail.com>
2018-06-22 19:07:42 +0200

    borderLineStyles container styles migrated

--- src/components/DynamicOutlines/index.js
+++ src/components/DynamicOutlines/index.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react'
 import './index.scss'
 
 export default class DynamicOutlines extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			clientHeight: 0,
+			clientWidth: 0
+		}
+	}
 	componentDidMount() {
 		const { itemId } = this.props;
 		if (itemId) {
-			const height = document.getElementById(this.props.itemId).clientHeight;
-			const width = document.getElementById(this.props.itemId).clientWidth;
-			this.setState({ height, width });
+			const clientHeight = document.getElementById(this.props.itemId).clientHeight;
+			const clientWidth = document.getElementById(this.props.itemId).clientWidth;
+			this.setState({ clientHeight, clientWidth });
 		} else {
 			console.log("must provide itemId in props");
 		}
@@ -22,11 +29,17 @@ export default class DynamicOutlines extends Component {
 			transitionTime
 		} = this.props;
 
+		const {
+			clientHeight,
+			clientWidth
+		} = this.state;
 
 		const borderLineStyles = {
 			position: 'absolute',
 			top: `-${(borderGap + (borderWidth/2))}px`,
 			left: `-${(borderGap + (borderWidth/2))}px`,
+			height: `${(clientHeight + (borderGap*2) + borderWidth)}px`,
+			width: `${(clientWidth + (borderGap*2) + borderWidth)}px`,
 		}
 
     const borderLines = ['side-a', 'side-b', 'side-c', 'side-d'].map((sideClass, idx) => (
