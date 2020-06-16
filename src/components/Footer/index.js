5464eb1dc7b044c84926d229ba86006abbaf506c
Anthony Castrio <anthonycastrio@gmail.com>
2020-06-16 16:32:55 -0400

    update copyright

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import './index.scss'
 
 const Footer = () => (
   <div className="footer">
-    <div className="copyright">Copyright 2018, Anthony Castrio</div>
+    <div className="copyright">Copyright 2020, Anthony Castrio</div>
   </div>
 );
 
